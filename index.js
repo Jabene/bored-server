7091a257dc825c1fe68707bbcc906ababc4598a5
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-09 21:19:23 -0700

    Change the port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server')
 
 const HOST = 'localhost';
-const PORT = 8080;
+const PORT = process.env.DATABASE_URL || 8080;
 
 server.listen(PORT, () => console.log(`Server running at ${HOST}:${PORT}`))
