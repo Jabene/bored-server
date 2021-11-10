ce340b5d7407dc5aa62171283e25ddf5c1d9e2d3
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-09 22:35:08 -0700

    Rebuild production connection

--- knexfile.js
+++ knexfile.js
@@ -16,16 +16,36 @@ module.exports = {
     },
     useNullAsDefault: true,
   },
-  
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
   production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL + '?ssl=true&ssl=no-verify',
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false},
+    },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
       directory: './api/migrations',
+      tableName: 'knex_migrations'
     },
     seeds: {
       directory: './api/seeds'
