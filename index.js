09e8576dc98837d915c95a3cfb78c015d147be1f
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-09 22:38:15 -0700

    Add port to server listen message

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const server = require('./api/server')
 
 server.set('port', process.env.PORT || 8080)
 
-server.listen(server.get('port'), () => console.log(`Server running`))
+server.listen(server.get('port'), () => console.log(`Server running on ${server.get('port')}`))
