f313c708622e3778788b09e54299548f33e3c80a
Joshua Benefield <benefield.joshua@gmail.com>
2021-11-11 00:18:03 -0700

    Comment in dev env

--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,42 @@
 // Update with your config settings.
 
 module.exports = {
-  // 
-  // development: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     connectionString: process.env.DATABASE_URL,
-  //     ssl: { rejectUnauthorized: false},
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     directory: './api/migrations',
-  //     tableName: 'knex_migrations'
-  //   },
-  //   seeds: {
-  //     directory: './api/seeds'
-  //   },
-  //   useNullAsDefault: true,
-  // },
-  //
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
+  
+  development: {
+    client: 'postgresql',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false},
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './api/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './api/seeds'
+    },
+    useNullAsDefault: true,
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
 
   production: {
     client: 'postgresql',
